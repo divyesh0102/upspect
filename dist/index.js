21fa03e2b9dc1e9d9352fe6d6bc8f6d028e2378e
Chintan Shah <chintan.shah@technostacks.com>
2022-06-17 11:47:23 +0530

    update fixes

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-Object.defineProperty(exports, "useViewport", {
-  enumerable: true,
-  get: function get() {
-    return _useViewport.useViewport;
-  }
-});
-
-var _useViewport = require("./useViewport");
+export { useViewport } from './useViewport';
--- dist/useViewport.js
+++ dist/useViewport.js
@@ -1,52 +1,18 @@
-"use strict";
-
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.useViewport = void 0;
-
-var React = _interopRequireWildcard(require("react"));
-
-var _lodash = require("lodash");
-
-var _defaultTheme = require("tailwindcss/defaultTheme");
-
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-var useViewport = function useViewport() {
-  var queries = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : _defaultTheme.screens;
-
-  var _React$useState = React.useState({
+import * as React from 'react';
+import { each, isArray, isPlainObject, isString, parseInt } from 'lodash';
+import { screens } from 'tailwindcss/defaultTheme';
+export const useViewport = (queries = screens) => {
+  const [viewportSize, setViewportSize] = React.useState({
     currentWidth: getWindowWidth(),
     currentHeight: getWindowHeight(),
     isMobileDevice: isMobileDevice(),
     activeBreakpoint: undefined
-  }),
-      _React$useState2 = _slicedToArray(_React$useState, 2),
-      viewportSize = _React$useState2[0],
-      setViewportSize = _React$useState2[1];
+  });
+  React.useEffect(() => {
+    let b; // breakpoint
 
-  React.useEffect(function () {
-    var b; // breakpoint
-
-    var handleResize = function handleResize() {
-      var w = getWindowWidth();
+    const handleResize = () => {
+      const w = getWindowWidth();
       /**
        * Evaluates `min` against the current screen width.
        *
@@ -55,9 +21,9 @@ var useViewport = function useViewport() {
        * @return {String}     Key of active breakpoint
        */
 
-      var min = function min(_min, k) {
-        _min = (0, _lodash.parseInt)(_min, 10);
-        b = w >= _min ? k : b;
+      const min = (min, k) => {
+        min = parseInt(min, 10);
+        b = w >= min ? k : b;
       };
       /**
        * Evaluates `max` against the current screen width.
@@ -68,9 +34,9 @@ var useViewport = function useViewport() {
        */
 
 
-      var max = function max(_max, k) {
-        _max = (0, _lodash.parseInt)(_max, 10);
-        b = w <= _max ? k : b;
+      const max = (max, k) => {
+        max = parseInt(max, 10);
+        b = w <= max ? k : b;
       };
       /**
        * Evaluates `min` and `max` against the current screen width.
@@ -82,9 +48,9 @@ var useViewport = function useViewport() {
        */
 
 
-      var rng = function rng(min, max, k) {
-        min = (0, _lodash.parseInt)(min, 10);
-        max = (0, _lodash.parseInt)(max, 10);
+      const rng = (min, max, k) => {
+        min = parseInt(min, 10);
+        max = parseInt(max, 10);
         b = w >= min && w <= max ? k : b;
       };
       /**
@@ -96,32 +62,26 @@ var useViewport = function useViewport() {
        */
 
 
-      var mlt = function mlt(q, k) {
+      const mlt = (q, k) => {
         if (q.min && !q.max) min(q.min, k);
         if (!q.min && q.max) max(q.max, k);
         if (q.min && q.max) rng(q.min, q.max, k);
       };
 
-      (0, _lodash.each)(queries, function (v, k) {
-        if ((0, _lodash.isString)(v)) {
+      each(queries, (v, k) => {
+        if (isString(v)) {
           // min
           min(v, k);
         }
 
-        if ((0, _lodash.isPlainObject)(v)) {
+        if (isPlainObject(v)) {
           // max
-          (0, _lodash.each)(v, function () {
-            return mlt(v, k);
-          });
+          each(v, () => mlt(v, k));
         }
 
-        if ((0, _lodash.isArray)(v)) {
+        if (isArray(v)) {
           // multi-range
-          (0, _lodash.each)(v, function (o) {
-            return (0, _lodash.each)(o, function () {
-              return mlt(o, k);
-            });
-          });
+          each(v, o => each(o, () => mlt(o, k)));
         }
       });
       setViewportSize({
@@ -134,14 +94,10 @@ var useViewport = function useViewport() {
 
     window.addEventListener('resize', handleResize);
     handleResize();
-    return function () {
-      return window.removeEventListener('resize', handleResize);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
   return viewportSize;
 };
-
-exports.useViewport = useViewport;
 
 function getWindowWidth() {
   return typeof window !== 'undefined' ? isMobileAgent() ? window.screen.width : window.innerWidth : 0;
